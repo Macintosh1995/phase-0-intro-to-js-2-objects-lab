991920e9ed86b1084a5cc237aaa218616da2fb0f
Daniel <danmcdonald95@outlook.com>
2023-07-11 23:22:22 -0700

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+
+let employee = {
+    name: "Sam"
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let obj = { ...employee };
+    obj[key] = value;
+    return obj;
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+  
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    let obj = { ...employee };
+    delete obj[key];
+    return obj;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key){
+      delete employee[key];
+      return employee;
+  
+  }
+  
+  
+  
+  //updateEmployeeWithKeyAndValue();
+  updateEmployeeWithKeyAndValue(employee, "height", "184");
+  destructivelyUpdateEmployeeWithKeyAndValue(employee, "nic", "Daniel");
